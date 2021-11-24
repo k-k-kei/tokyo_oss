777600461ea8333c4505af0495bb88f40d47cc86
keita_kawaguchi <keikawagu@gmail.com>
2021-11-24 10:08:33 +0900

    firebase設定完了

--- front/nuxt_app/components/Hello.vue
+++ front/nuxt_app/components/Hello.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
-        <div>Hello</div>
+        <div v-for="(text, index) in texts" :key="index">{{ text }}</div>
     </div>
 </template>
+
+<script lang="ts">
+import { ref, defineComponent, onMounted } from '@vue/composition-api'
+import db from '~/plugins/firebase';
+
+export default defineComponent({
+    setup() {
+        const texts = ref<Array<String>>([]);;
+
+        onMounted(() => {
+            db.collection("memo").get().then((querySnapshot: any) => {
+                querySnapshot.forEach((doc: any) => {
+                    console.log(doc.data().text);
+                    texts.value.push(doc.data().text);
+                })
+            })
+        })
+
+        return{
+            texts
+        }
+        
+    },
+})
+</script>
---
+++ front/nuxt_app/plugins/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_PROJECT_API,
+  authDomain: process.env.FIREBASE_AUTH_DOMEIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STRAGE_BUCKET
+};
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig)
+}
+const db = firebase.firestore();
+export default db;
