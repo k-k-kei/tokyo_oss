ec494dd57e97082c4c608b31a8c5109e84bf4a0a
r0ckandc0de <yohtaro.s@gmail.com>
2021-11-27 18:10:46 +0900

    create default

---
+++ front/nuxt_app/components/LayoutHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header id="header" class="pt-5">
+        <p
+          class="text-center text-sm font-bold leading-relaxed"
+        >
+          Gs' FRIENDS
+        </p>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+
+    },
+})
+</script>
