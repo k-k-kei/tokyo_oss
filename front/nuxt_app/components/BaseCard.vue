a62d3b6ecb27a8f6805b448b72ba36e7509d0400
r0ckandc0de <yohtaro.s@gmail.com>
2021-11-30 15:45:44 +0900

    maintain index.vue for firebase

--- front/nuxt_app/components/BaseCard.vue
+++ front/nuxt_app/components/BaseCard.vue
@@ -1,33 +1,36 @@
 <template>
     <div class="flex flex-col md:grid md:grid-cols-4 md:gap4 2xl:grid-cols-6">
-      <div v-for="card in cards" :key="card.id" class="flex flex-col mx-auto pt-4 pb-2 border-b-2 md:border-b-0 px-4">
-        <NuxtLink :to="'/' + card.id + '/'">
-            <div class="w-full">
-                <img class="max-w-full rounded-md" :src="card.img" :alt="card.title">
+      <div v-for="(memo, i) in cards" :key="i" class="w-full flex flex-col mx-auto pt-4 pb-2 border-b-2 md:border-b-0 px-4">
+        <NuxtLink :to="'/' + i + '/'">
+            <div v-if="memo.mainImage" class="w-full">
+                <img class="max-w-full object-cover h-45 rounded-md mx-auto" :src="memo.mainImage" :alt="memo.title">
+            </div>
+            <div v-else class="w-full">
+                <img class="max-w-full object-cover h-45 rounded-md mx-auto" src="/images/sotoburo.png" :alt="memo.title">
             </div>
             <div class="mt-2">
-                <h2 class="text-lg text-cMain tracking-wider font-bold line-clamp-2">{{ card.title }}</h2>
+                <h2 class="text-lg text-cMain tracking-wider font-bold line-clamp-2">{{ memo.title }}</h2>
             </div>
             <div class="mt-2">
-                <h3 class="text-base text-cGray tracking-wider line-clamp-2 md:line-clamp-2">{{ card.text }}</h3>
+                <h3 class="text-base text-cGray tracking-wider line-clamp-2 md:line-clamp-2">{{ memo.blocks[0].data.text }}</h3>
             </div>
             <div class="mt-2 flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-cPink" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
-                <span class="text-sm text-cPink ml-1 tracking-wider line-clamp-2 md:line-clamp-4">{{ card.like }}</span>
+                <span class="text-sm text-cPink ml-1 tracking-wider line-clamp-2 md:line-clamp-4">{{ likeNum }}</span>
             </div>
             <div class="mt-2">
                 <div class="flex items-center">
                   <div>
-                    <img class="max-w-full w-8 rounded-full" :src="card.editorImage" :alt="card.editor">
+                    <img class="max-w-full w-8 rounded-full" :src="editorImage" :alt="editorImage">
                   </div>
                   <div class="flex flex-col ">
                     <div class="text-xs text-cGray pl-2">
-                      <p>{{ card.editor}}</p>
+                      <p>{{ editorName }}</p>
                     </div>
                     <div class="text-xs text-cGray pl-2">
-                      <p>{{ card.created }}</p>
+                      <p>{{ created }}</p>
                     </div>
                   </div>
                 </div>
@@ -39,6 +42,7 @@
 
 <script lang="ts">
 import { defineComponent, computed } from '@nuxtjs/composition-api'
+import { reverse } from 'dns'
 
 export default defineComponent({
   props: {
@@ -49,8 +53,13 @@ export default defineComponent({
   },
   setup(props) {
     const cardNum = computed(() => props.cards.length)
+    const likeNum = 42
+    const editorImage = "https://www.himalmag.com/wp-content/uploads/2019/07/sample-profile-picture.png"
+    const editorName = "BIRD"
+    const created = '2021,11,3'
+
     return {
-      cardNum
+      cardNum, likeNum, editorImage, editorName, created
     }
   },
 })
