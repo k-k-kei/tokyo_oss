52b8c2d297ffb29d3c3b7b6b31a783dd5f922ddd
shinji-kodama <shinji19851208@gmail.com>
2021-12-01 20:30:56 +0900

    保存ができないよぉ・・・

--- front/nuxt_app/pages/edit/index.vue
+++ front/nuxt_app/pages/edit/index.vue
@@ -1,9 +1,20 @@
 <template class="bg-cWhite">
-  <ArticleEdit />
+  <ArticleEdit :id="route"/>
 </template>
 
-<script>
-export default {
-  middleware: 'authCheck'
-}
+<script lang="ts">
+import { ref, defineComponent, useRoute, onMounted } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  middleware: 'authCheck',
+  setup(){
+    const route = useRoute()
+
+    onMounted(() => {
+      console.log(route)
+    })
+  },
+
+
+})
 </script>
