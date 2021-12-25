269e73c8e2d6a54daaa52318d72d20edf8636efe
chell2 <rei1259@gmail.com>
2021-12-25 13:22:03 +0900

    Adding Links

--- front/nuxt_app/components/LayoutFooter.vue
+++ front/nuxt_app/components/LayoutFooter.vue
@@ -1,19 +1,30 @@
 <template>
-    <footer id="footer" class="bg-cBase pt-20 pb-20 md:pb-5 pt-5 md:pt-32">
+  <footer id="footer" class="flex justify-center bg-cBase pt-6 md:pt-10 pb-20 md:pb-8 md:w-full">
+    <div class="space-y-3 w-4/5">
+      <div class="block text-cGray text-center leading-8">
+        <Nuxt-link to="/" class="hover:text-cMain">
+          ホーム
+        </nuxt-link>
+        ｜
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLScTc50sPg9_7MUi1fioy8R5PvIIHLiln-cxcnvs2rncR6oeAw/viewform" class="hover:text-cMain">お問い合わせ</a>
+      </div>
+      <div class="block">
         <p
           class="text-center text-cMain text-sm font-bold leading-relaxed"
         >
-          Copyright © 2021 Gs' FRIENDS
+          Copyright © 2021 GsFRIENDS
         </p>
-    </footer>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
-    setup() {
+  setup () {
 
-    },
+  }
 })
 </script>
