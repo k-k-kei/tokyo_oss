d58530b26a1d0097b4bc0205e85c5c4b4c4c57d2
chell2 <rei1259@gmail.com>
2022-01-03 17:39:50 +0900

    Modify my page

--- front/nuxt_app/pages/notes.vue
+++ front/nuxt_app/pages/notes.vue
@@ -1,19 +1,50 @@
 <template>
-  <article-list-wrapper>
-    <div>
-      <ArticleList :articles="memos" />
-    </div>
-    <div class="text-center pt-12">
-      <NuxtLink to="/login">
-        <button class="bg-cGray rounded">
-          <div class="text-cBase px-4 py-2 text-xs md:text-sm">
-            <span class="text-cBase font-semibold">{{ buttonTitle }}</span>
+  <main class="flex justify-center p-10 bg-cBlueGray border border-concreteGray">
+    <div class="flex items-start justify-center w-full m-0">
+      <!-- <article-list-wrapper> -->
+      <div class="flex">
+        <div class="mb-4">
+          <h1 class="text-xl font-bold w-40 leading-6 mb-8 ml-3">
+            記事
+          </h1>
+          <ul class="pb-4 mb-4">
+            <li :class="{ clickBtn: tab === 1 }" @click="tab = 1">
+              すべて
+            </li>
+            <li :class="{ clickBtn: tab === 2 }" @click="tab = 2">
+              公開中
+            </li>
+            <li :class="{ clickBtn: tab === 3 }" @click="tab = 3">
+              下書き
+            </li>
+          </ul>
+        </div>
+        <div class="content">
+          <div v-show="tab === 1" class="content-item">
+            <ArticleList :articles="memosAll" />
           </div>
-        </button>
-      </NuxtLink>
+          <div v-show="tab === 2" class="content-item">
+            <ArticleList :articles="memosPublic" />
+          </div>
+          <div v-show="tab === 3" class="content-item">
+            <ArticleList :articles="memosDraft" />
+          </div>
+
+          <div class="text-center pt-12">
+            <NuxtLink to="/login">
+              <button class="bg-cGray rounded">
+                <div class="text-cBase px-4 py-2 text-xs md:text-sm">
+                  <span class="text-cBase font-semibold">{{ buttonTitle }}</span>
+                </div>
+              </button>
+            </NuxtLink>
+          </div>
+        </div>
       <!-- <base-button  :title="buttonTitle" :link="buttonLink" @buttonEvent="logout" /> -->
+      </div>
     </div>
-  </article-list-wrapper>
+  </main>
+  <!-- </article-list-wrapper> -->
 </template>
 
 <script lang="ts">
@@ -23,7 +54,9 @@ import { db, auth } from '../plugins/firebase'
 export default defineComponent({
   middleware: 'authCheck',
   setup () {
-    const memos = ref<any[]>([])
+    const memosAll = ref<any[]>([])
+    const memosPublic = ref<any[]>([])
+    const memosDraft = ref<any[]>([])
     const userId = ref('')
 
     auth.onAuthStateChanged((user:any) => {
@@ -43,16 +76,80 @@ export default defineComponent({
           querySnapshot.forEach((doc: any) => {
             if (doc.data().uid === userId.value) {
               const dataArray = Object.assign(doc.data(), { id: doc.id })
-              memos.value.push(dataArray)
+              memosAll.value.push(dataArray)
+            }
+          })
+        })
+      db.collection('memo')
+        .where('isPublic', '==', true)
+        .get()
+        .then((querySnapshot: any) => {
+          querySnapshot.forEach((doc: any) => {
+            if (doc.data().uid === userId.value) {
+              const dataObj = Object.assign(doc.data(), { id: doc.id })
+              memosPublic.value.push(dataObj)
+              memosPublic.value.sort((memo1, memo2) => memo1.time > memo2.time ? -1 : 1)
+            }
+          })
+        })
+      db.collection('memo')
+        .where('isPublic', '==', false)
+        .get()
+        .then((querySnapshot: any) => {
+          querySnapshot.forEach((doc: any) => {
+            if (doc.data().uid === userId.value) {
+              const dataObj = Object.assign(doc.data(), { id: doc.id })
+              memosDraft.value.push(dataObj)
+              memosDraft.value.sort((memo1, memo2) => memo1.time > memo2.time ? -1 : 1)
             }
           })
         })
     })
+
     const buttonTitle = 'ログアウト'
     return {
       buttonTitle,
-      memos
+      memosAll,
+      memosPublic,
+      memosDraft,
+      tab: 1
     }
   }
 })
 </script>
+<style scoped>
+ul {
+  list-style: none;
+  margin-top: 20px;
+}
+li {
+  color: cBlack;
+  width: 160px;
+  height: 40px;
+  padding: 8px;
+  margin: 2px;
+  position: relative;
+}
+li:hover{
+  background-color: rgb(229 231 235);
+  border-radius: 3px;
+}
+.content {
+  margin-left: 10px;
+}
+.clickBtn {
+  font-weight: bold;
+}
+.clickBtn::before {
+  content: '';
+  position: absolute;
+  border-color: #474655;
+  background-color: #474655;
+  border-style: solid;
+  border-width: 1px;
+  height: 40px;
+  margin-left: -10px;
+  margin-top: -8px;
+  width: 0px;
+}
+</style>
