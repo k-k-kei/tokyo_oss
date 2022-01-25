966c33ffc293f1491d2361bfdce87a7392e8d0d4
keita_kawaguchi <keikawagu@gmail.com>
2022-01-25 21:44:53 +0900

    リンクテキストの修正

--- front/nuxt_app/components/ArticleContent.vue
+++ front/nuxt_app/components/ArticleContent.vue
@@ -36,7 +36,7 @@
       </div>
       <!-- ユーザー名と日時 -->
       <div class="mt-4">
-        <p class="text-sm">{{ userId }}</p>
+        <p class="text-sm">{{ userName }}</p>
         <p class="text-sm text-gray-300">{{ changeDate(datetime) }}</p>
       </div>
       <!-- 本文 -->
@@ -46,47 +46,16 @@
           <p class="text-center text-sm text-cGray p-2">{{ text.data.caption }}</p>
         </div>
         <div v-else>
-          <p :class="textStyle(text.type)">
-            {{ text.data.text }}
-          </p>
+          <div :class="textStyle(text.type)">
+            <div v-if="text.data.text.includes('http')">
+              <a :href="text.data.text" class="text-blue-700">{{ text.data.text }}</a>
+            </div>
+            <div v-else>
+              <div>{{ text.data.text }}</div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <!-- 機能つけきれないので一旦コメントアウト -->
-      <!-- 関連タグ -->
-      <!-- <div class="flex flex-wrap text-sm">
-                <span class="mr-2 p-2 border rounded-md">#車椅子でも快適</span>
-                <span class="mr-2 p-2 border rounded-md">#カフェ</span>
-            </div>
-        </div> -->
-      <!-- 記事リコメンド -->
-      <!-- <div class="border-t border-b border-gray-100 py-7"> -->
-      <!-- タイトル -->
-      <!-- <p class="font-bold">人気記事</p> -->
-
-      <!-- 記事① -->
-      <!-- <div class="mt-4">
-                <p class="font-bold text-sm">コーヒーを美味しく入れる方法</p>
-                <div class="flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                    <span class="text-pink-500 text-sm pl-1">22</span>
-                    <p class="text-sm text-gray-300 pl-3">コーヒー侍</p>
-                </div>
-            </div> -->
-
-      <!-- 記事② -->
-      <!-- <div class="mt-4">
-                <p class="font-bold text-sm">美味しいお菓子の見分け方</p>
-                <div class="flex">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                    <span class="text-pink-500 text-sm pl-1">22</span>
-                    <p class="text-sm text-gray-300 pl-3">コーヒー侍</p>
-                </div>
-            </div> -->
     </div>
   </div>
 </template>
@@ -113,6 +82,9 @@ export default defineComponent({
     const like = ref<number>();
     //記事作成日時
     const datetime = ref("");
+    
+    //firestoreのusersコレクションに登録されているユーザー一覧
+    const userName = ref("");
 
     // firebaseのオブジェクトをリアルタイムに取得
     onMounted(() => {
@@ -136,6 +108,16 @@ export default defineComponent({
               //配列を展開して一つずつ上記で定義したdataに移す
               const dataArray = doc.data().blocks;
               dataArray.forEach((el:any) => data.value.push(el));
+            }
+          });
+        });
+
+      db.collection("users")
+        .get()
+        .then((querySnapshot: any) => {
+          querySnapshot.forEach((doc: any) => {
+            if (doc.data().uid === userId.value) {
+              userName.value = doc.data().name;
             }
           });
         });
@@ -165,10 +147,10 @@ export default defineComponent({
       userId,
       like,
       datetime,
+      userName,
       textStyle,
       changeDate,
-      addLike
-      // getImageFile
+      addLike,
     };
   },
 });
